0f5b9838962a8eb05cd7f590734dc27043f1a7b0
Mohit Manucha <manucha23@gmail.com>
2023-08-15 18:03:23 +0530

    basic hello world

---
+++ src/main/java/com/manucha/aws/helloworld/HelloController.java
@@ -0,0 +1,13 @@
+package com.manucha.aws.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/helloworld")
+    public String get(){
+        return "Hello World";
+    }
+}
