0c2a487437978d4411e1bf5df3af8b643688a461
Mohit Manucha <manucha23@gmail.com>
2023-08-20 15:08:16 +0530

    Printing IP

--- src/main/java/com/manucha/aws/helloworld/HelloController.java
+++ src/main/java/com/manucha/aws/helloworld/HelloController.java
@@ -3,11 +3,15 @@ package com.manucha.aws.helloworld;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/helloworld")
-    public String get(){
-        return "Hello World";
+    public String get() throws UnknownHostException {
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        return "Hello World from: " + ip;
     }
 }
