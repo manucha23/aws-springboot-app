a4bd47c48d3a2f7db2e958a9041b50619eba219c
Mohit Manucha <manucha23@gmail.com>
2023-08-21 19:12:52 +0530

    Server port changed to default 8080

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -jar -Dserver.port=80 /webapps/hello-world-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+java -jar /webapps/hello-world-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
